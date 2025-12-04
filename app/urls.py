bce34fb488080d9237b4024bc3b58b9319e6577b
Christian B. Baldemoro <chbaldemoro@gbox.adnu.edu.ph>
2025-12-04 13:14:33 +0800

    Added Settings Screen

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('directory/', views.directory_view, name='directory'),
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    
-    
+    path('settings/', views.settings_view, name='settings'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,13 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def homepage(request):
-    return render(request, 'index.html')
-def directory_view(request):
-    return render(request, 'directory.html')
-
-def signup_view(request):
-    return render(request, 'signup.html')
-def login_view(request):
-    return render(request, 'login.html')
-
+def settings_view(request):
+    return render(request, 'settings.html')

d2280a573f0ec06e6de61b51a9f17e47f1c5d92e
stubbierbread30 <dave.asico@gmail.com>
2025-12-06 18:45:50 +0800

    added .env

--- goldenpath/settings.py
+++ goldenpath/settings.py
@@ -10,17 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+)guvg=70&theo43)bqx4_+p8o7aar*t#vebm2g-a*j_naa@*d'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,11 +78,11 @@ WSGI_APPLICATION = 'goldenpath.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "goldenpath",
-        "USER": "integ2",
-        "PASSWORD": "testing",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 

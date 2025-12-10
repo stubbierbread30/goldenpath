11ca7b9ee09d5eff62a42c6a89300fdb44343067
stubbierbread30 <dave.asico@gmail.com>
2025-12-10 15:47:15 +0800

    Fixed staticfiles for deployment #2

--- goldenpath/settings.py
+++ goldenpath/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
